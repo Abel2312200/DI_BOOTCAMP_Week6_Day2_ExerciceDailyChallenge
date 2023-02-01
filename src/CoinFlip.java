01a4cc02ac629b4d2792d86a504e28a184ca41ad
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-02-01 02:36:20 +0000

    Daily challenge done

--- src/CoinFlip.java
+++ src/CoinFlip.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CoinFlip {
+import java.util.Random;
+public class CoinFlip {
+    public static void main(String[] args) {
+        // create a new random number generator object
+        Random rand = new Random();
+
+        // pick a random number between 0 and 1:
+        int n = rand.nextInt(2);
+        System.out.println("Picked random value " + n);
+
+        // display heads or tails according to n variable
+        if(n == 0){
+            System.out.println("Heads");
+        }else{
+            System.out.println("Tails");
+        }
+    }
 }
